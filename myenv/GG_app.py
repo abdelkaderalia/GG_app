1b05cbca51985c1b521ce8a38c382df6d2cdb0ca
Alia Abdelkader <alia@Alias-MacBook-Pro.local>
2022-08-29 23:46:26 -0400

    Formatting adjustments

--- myenv/GG_app.py
+++ myenv/GG_app.py
@@ -6,6 +6,7 @@ import os
 import xlrd
 import openpyxl
 import requests
+import numpy
 
 @st.cache(show_spinner=False)
 def episode_list():
@@ -24,18 +25,13 @@ def return_episode(df,mood_name):
 
 #### App starts here
 if __name__ == "__main__":
-    st.markdown('<h2 align="center">Gilmore Girls Episode Selector</h2>', unsafe_allow_html=True) # Add app title
-
-    moods = ['','Happy','Sad','Angsty','Funny']
-
-    col1, col2 = st.columns(2)
-    mood_name = col1.selectbox("Choose your mood:", moods) # Store user selection for agency name
-
-
-    st.title('')
-
+    #st.markdown('<h2 align="center">Gilmore Girls Episode Selector</h2>', unsafe_allow_html=True) # Add app title
+    st.title('Gilmore Girls Episode Selector')
     df = episode_list()
+    moods = df['Category'].unique().tolist()
+    moods.insert(0,'')
+    mood_name = st.sidebar.selectbox("Choose your mood:", moods) # Store user selection for agency name
+
     if mood_name!='':
         selector = return_episode(df,mood_name)
-        col2.title('')
-        col2.write(selector)
+        st.success(selector)
